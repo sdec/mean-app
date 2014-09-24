864c947cdb8444466906f16380eea610b8a5ef34
sander <sander@sander>
2014-09-24 12:33:57 +0200

    Initial commit, fresh MEAN.JS installation with database

---
+++ public/modules/core/controllers/home.client.controller.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+angular.module('core').controller('HomeController', ['$scope', 'Authentication',
+	function($scope, Authentication) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+
+		$scope.alerts = [
+		  {
+		    "icon": 'glyphicon-user',
+		    "color": 'btn-success',
+		    "total": '20,408',
+		    "description": 'TOTAL CUSTOMERS'
+		  },
+		  {
+		    "icon": 'glyphicon-calendar',
+		    "color": 'btn-primary',
+		    "total": '10,808',
+		    "description": 'UPCOMING EVENTS'
+		  },
+		  {
+		    "icon": 'glyphicon-edit',
+		    "color": 'btn-success',
+		    "total": '4,419',
+		    "description": 'NEW CUSTOMERS IN 24 HOURS'
+		  },
+		  {
+		    "icon": 'glyphicon-record',
+		    "color": 'btn-info',
+		    "total": '17,155',
+		    "description": 'EMAILS SENT'
+		  },
+		  {
+		    "icon": 'glyphicon-eye-open',
+		    "color": 'btn-warning',
+		    "total": '4,830',
+		    "description": 'FOLLOW-UPS REQUIRED'
+		  },
+		  {
+		    "icon": 'glyphicon-flag',
+		    "color": 'btn-danger',
+		    "total": '19,286',
+		    "description": 'REFERRALS TO MODERATE'
+		  }
+		];
+	}
+]);
